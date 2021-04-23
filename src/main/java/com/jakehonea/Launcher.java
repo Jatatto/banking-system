68928787a55679f882c8090114cfd01e0c279177
Jake H <honeajake@yahoo.com>
2021-04-22 23:51:33 -0500

    Fixed Launcher class

--- src/main/java/com/jakehonea/Launcher.java
+++ src/main/java/com/jakehonea/Launcher.java
@@ -1,7 +1,6 @@
 package com.jakehonea;
 
 import com.jakehonea.banking.CentralBank;
-import com.jakehonea.ui.AccountInformationUI;
 import com.jakehonea.ui.LoginUI;
 
 import java.io.IOException;
@@ -11,8 +10,7 @@ public class Launcher {
     public static void main(String[] args) {
 
         try {
-            new AccountInformationUI(new CentralBank().getAccountManager().getAccount("Fortnite"));
-            //new LoginUI(new CentralBank());
+            new LoginUI(new CentralBank());
         } catch (IOException e) {
             e.printStackTrace();
         }
